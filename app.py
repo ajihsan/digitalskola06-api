d3eed14e5b8c5b1dffc43302522f22fcf0f8dee5
ajihsan <ajihsan96@gmail.com>
2022-04-17 23:34:07 +0700

    change method

--- app.py
+++ app.py
@@ -1,30 +1,43 @@
 from flask import Flask, jsonify, request
-from db.query_raw import connection
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+DB_URI = "postgresql+psycopg2://digitalskola:D6GhCbaaiq8LlNy7@35.193.53.27:5432/api"
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+  __table_args__ = {"schema": "aji"}
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  address = db.Column(db.String)
+
+  def __init__(self, name, address):
+    self.name = name
+    self.address = address
+
 
 @app.route("/users", methods=["GET", "POST"])
 def users():
     if request.method == 'GET':
-        query = """
-        SELECT * FROM aji.users
-        """
-        conn, cursor = connection()
-        cursor.execute(query)
-        result = cursor.fetchall()
+        users = Users.query.all()
+        result = [
+            {
+                "name": user.name,
+                "model": user.address
+            } for user in users]
         return jsonify(result)
     
     elif request.method == 'POST':
-        name = request.args.get('name')
-        address = request.args.get('address')
-        query = f"""
-        INSERT INTO aji.users (name, address)
-        VALUES ('{name}', '{address}')
-        """
-        conn, cursor = connection()
-        cursor.execute(query)
-        conn.commit()
-        return {"message": f"Users {name} has been created successfully."}
+        if request.is_json:
+            data = request.get_json()
+            new_users = Users(name=data['name'], address=data['address'])
+            db.session.add(new_users)
+            db.session.commit()
+            return {"message": f"User {new_users.name} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
 
 
 if __name__ == "__main__":
